30285ee627677b98df83b0334df25f46d07f0aaf
jonathan gatti <jonathangatti2@gmail.com>
2020-09-26 18:21:39 +0100

    styled lamp list

--- src/LampList.js
+++ src/LampList.js
@@ -12,32 +12,35 @@ import Typography from '@material-ui/core/Typography';
 
 
 const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexBasis: '25%',
+  },
   root: {
     flexGrow: 1,
-    backgroundColor: 'black',
-    color: 'white',
-    display: 'flex',
-    flexWrap: 'wrap',
-    margin: '1rem',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
+    margin: '3rem',
+    backgroundColor: 'rgba(255,255,255,0.3)',
+    width: '300px',
   },
   media: {
-    maxWidth: '100%',
-    maxHeight: '100%'
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%'
   },
   img: {
-    maxHeight: '300px',
+    width: '300px',
+    height: '300px',
     maxWidth: '100%',
-    objectFit: 'cover'
+    maxHeight: '100%',
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+  link: {
+    textDecoration: 'none',
+    color: 'black',
+    marginLeft: '0.35em',
+    fontSize: '1.5em'
+  }
 }));
 
 export default function LampList(props) {
@@ -51,39 +54,36 @@ export default function LampList(props) {
       return lampType
     }
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        {getType(type).map(lamp => (
-          <Grid item xs={6} md={2}>
-            <Card className={classes.root}>
-              <CardMedia  
-                className={classes.media}>
-                <img className={classes.img} 
-                     src={require(`./imgs/${lamp.src}`)} alt='lamp' />
-              </CardMedia>
-              <CardActionArea>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {lamp.name}
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    {lamp.description}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  Share
-                </Button>
-                <Link className='Lamp-title'
-                      to={`/lamps/${lamp.type}/${lamp.name}`} >
-                      Discover More
-                </Link>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+    <div className={classes.container}>
+        <Grid container spacing={3}>
+          {getType(type).map(lamp => (
+            <Grid item md={6} lg={4} xl={3}>
+              <Card className={classes.root}>
+                <CardMedia  
+                  className={classes.media}>
+                  <img className={classes.img} 
+                      src={require(`./imgs/${lamp.src}`)} alt='lamp' />
+                </CardMedia>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {lamp.name}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      {lamp.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Link className={classes.link}
+                        to={`/lamps/${lamp.type}/${lamp.name}`} >
+                        Discover More
+                  </Link>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -12,6 +12,9 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
+  navbar: {
+    backgroundColor: 'black'
+  },
   menuButton: {
     marginRight: theme.spacing(2),
   },
@@ -25,7 +28,7 @@ export default function ButtonAppBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar className={classes.navbar} position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
