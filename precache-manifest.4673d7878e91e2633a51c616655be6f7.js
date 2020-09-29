22880254aec692de710e24ba7c0c854193925a8c
jonathan gatti <jonathangatti2@gmail.com>
2020-09-29 20:06:07 +0100

    Updates

---
+++ precache-manifest.4673d7878e91e2633a51c616655be6f7.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9193f8df047411baec64f680f239bb9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21dacec61284f85f8fb1",
+    "url": "/static/css/main.7782e9ce.chunk.css"
+  },
+  {
+    "revision": "260f93972caa35aac08b",
+    "url": "/static/js/2.9dc7b072.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9dc7b072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21dacec61284f85f8fb1",
+    "url": "/static/js/main.f3a3bc5c.chunk.js"
+  },
+  {
+    "revision": "2ea2ccc15d8a6e488bcd",
+    "url": "/static/js/runtime-main.60a6c9b1.js"
+  },
+  {
+    "revision": "c320677883302676480def496445b7f9",
+    "url": "/static/media/alianor.c3206778.jpg"
+  },
+  {
+    "revision": "5bc0b95ee607e634b2ef601dfae0c1d4",
+    "url": "/static/media/alu.5bc0b95e.jpg"
+  },
+  {
+    "revision": "2ad7b0d2ed25a155457bba5bd5cdc8ff",
+    "url": "/static/media/bident.2ad7b0d2.jpg"
+  },
+  {
+    "revision": "c5caa040bc1fbd9a48cc008e15f28d6d",
+    "url": "/static/media/blow.c5caa040.jpg"
+  },
+  {
+    "revision": "070d9f583b2961d4ae5f79646d59d32c",
+    "url": "/static/media/camel.070d9f58.jpg"
+  },
+  {
+    "revision": "0812fc33f953c7d5d463427ee7c88e36",
+    "url": "/static/media/cirene.0812fc33.jpg"
+  },
+  {
+    "revision": "8123bf749e95d822174dcbacbaddbc85",
+    "url": "/static/media/creusa.8123bf74.jpg"
+  },
+  {
+    "revision": "a1bcaba4c9f7b9a82ed2e1566094df15",
+    "url": "/static/media/diko1.a1bcaba4.jpg"
+  },
+  {
+    "revision": "4870a30200b9579a829a243962523e8a",
+    "url": "/static/media/diko2.4870a302.jpg"
+  },
+  {
+    "revision": "0a526dbc52ba91af6df5b3c132136905",
+    "url": "/static/media/diko3.0a526dbc.jpg"
+  },
+  {
+    "revision": "c1e23a394d8afe5fe527051e92b1fb6d",
+    "url": "/static/media/elf.c1e23a39.jpg"
+  },
+  {
+    "revision": "e64ce9b7af4a415c54c741e8f4c9094b",
+    "url": "/static/media/fossil.e64ce9b7.jpg"
+  },
+  {
+    "revision": "f0a8f300693cc6ccee1c3c160b7eb8b8",
+    "url": "/static/media/hadrian.f0a8f300.jpg"
+  },
+  {
+    "revision": "c97569e8aef860a80237da3a8ad87443",
+    "url": "/static/media/homepage.c97569e8.jpg"
+  },
+  {
+    "revision": "2ef38bd67f1a627817cadc335be343bc",
+    "url": "/static/media/kalias.2ef38bd6.jpg"
+  },
+  {
+    "revision": "c14a2dfdc4d27baeb07e7d51f9e7e321",
+    "url": "/static/media/krea.c14a2dfd.jpg"
+  },
+  {
+    "revision": "a8637542f569046bc535ac45d1e3e8f5",
+    "url": "/static/media/kroner.a8637542.jpg"
+  },
+  {
+    "revision": "ac094d98a5e84d078694135ad8e3a99a",
+    "url": "/static/media/lucrezia.ac094d98.jpg"
+  },
+  {
+    "revision": "4ad2e7549d6a46dbac4ee28c6c929a82",
+    "url": "/static/media/mante.4ad2e754.jpg"
+  },
+  {
+    "revision": "6819b411c4763913409a8096d9b95c4b",
+    "url": "/static/media/medusa.6819b411.jpg"
+  },
+  {
+    "revision": "f2b2eb9456af7e476503dcff2ae73ce0",
+    "url": "/static/media/moon.f2b2eb94.jpg"
+  },
+  {
+    "revision": "d7e6321554159c77d7633ba24f26dd03",
+    "url": "/static/media/nest.d7e63215.jpg"
+  },
+  {
+    "revision": "da287df48acc95233b944dfbbe72fc04",
+    "url": "/static/media/onetotwelve.da287df4.jpg"
+  },
+  {
+    "revision": "d445d11bd8f579dede6341a7b4513ec3",
+    "url": "/static/media/pippa.d445d11b.jpg"
+  },
+  {
+    "revision": "180dd32a9199a34118f4b58aa882e328",
+    "url": "/static/media/plares.180dd32a.jpg"
+  },
+  {
+    "revision": "c3f47b79b04902261df1c0455b137b75",
+    "url": "/static/media/red.c3f47b79.jpg"
+  },
+  {
+    "revision": "37759f2d213da9fed285fbf3aa9b5009",
+    "url": "/static/media/rialto.37759f2d.jpg"
+  },
+  {
+    "revision": "889185a30e995aff6c66c7c1a0d2c69a",
+    "url": "/static/media/samurai.889185a3.jpg"
+  },
+  {
+    "revision": "bc235d238e9d2a3afbdac2ff4032dd9a",
+    "url": "/static/media/scooby.bc235d23.jpg"
+  },
+  {
+    "revision": "0bd334530f6402b9fadceaedbbcb01ee",
+    "url": "/static/media/sieving.0bd33453.jpg"
+  },
+  {
+    "revision": "40008b92e7e965454b7896642349af16",
+    "url": "/static/media/silversun.40008b92.jpg"
+  },
+  {
+    "revision": "ad8593993e5e2edb848a89d9e9c1882c",
+    "url": "/static/media/spiral.ad859399.jpg"
+  },
+  {
+    "revision": "1ae77abf398b749153929a439c33c67e",
+    "url": "/static/media/spiralis.1ae77abf.jpg"
+  },
+  {
+    "revision": "e94acd8971deaddfe2f8505609887993",
+    "url": "/static/media/starlight.e94acd89.jpg"
+  },
+  {
+    "revision": "5bdb76c4d553202a25b4ed36edf7e335",
+    "url": "/static/media/sun.5bdb76c4.jpg"
+  },
+  {
+    "revision": "69f6ca9a5307f317690ef0d024239fbd",
+    "url": "/static/media/sunset.69f6ca9a.jpg"
+  },
+  {
+    "revision": "d0ab48b9dd9e4c2782966320830ef287",
+    "url": "/static/media/timeless.d0ab48b9.jpg"
+  },
+  {
+    "revision": "abcb801c487916eaf542a7245d089b27",
+    "url": "/static/media/trident.abcb801c.jpg"
+  },
+  {
+    "revision": "16c3ca346d5f99ba5df24a357a111158",
+    "url": "/static/media/utube.16c3ca34.jpg"
+  },
+  {
+    "revision": "e66d074e1105b2203336f50d8327b32c",
+    "url": "/static/media/worldsend.e66d074e.jpg"
+  }
+]);
